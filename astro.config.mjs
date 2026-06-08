35fab96b43be5804a6a9b8c5ae2afbae3ccbc710
Yago Lizarribar <yago.lizarribar@gmail.com>
2026-06-08 10:21:26 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://yagolc.me",
+  integrations: [sitemap()],
 });
